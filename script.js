67c51e3dc0dea9083d5dfa75111e50286d5524bb
Fengxing WEI <86094248+fxwei@users.noreply.github.com>
2022-06-30 23:35:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,301 @@
+class NumberSlot {
+    constructor(){
+
+        this.bigCardPool = [25,50,75,100];
+        this.smallCardPool = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+
+        this.target = 100;
+        this.slot = [0,0,0,0,0,0];
+        this.isPrim = [true, true, true, true, true, true];
+        this.slotElement = document.getElementsByClassName("number");
+    }
+
+    shuffle(){
+        for (let i = this.bigCardPool.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.bigCardPool[i];
+            this.bigCardPool[i] = this.bigCardPool[j];
+            this.bigCardPool[j] = temp;
+        }
+        for (let i = this.smallCardPool.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.smallCardPool[i];
+            this.smallCardPool[i] = this.smallCardPool[j];
+            this.smallCardPool[j] = temp;
+        }
+    }
+
+    retry(){
+        for (let i = 0; i<6; i++){
+            this.slotElement[i].innerHTML = this.slot[i];
+        }
+    }
+
+    setSlot(numBigCards){
+        if (numBigCards == -1){
+            numBigCards = Math.floor(Math.random() * 5)
+        }
+        this.shuffle();
+        this.slot = this.bigCardPool.slice(0, numBigCards).concat(this.smallCardPool.slice(0, 6 - numBigCards));
+
+        let targetElement = document.getElementsByClassName("target")[0];
+        for (let i = 0; i<6; i++){
+            this.slotElement[i].innerHTML = this.slot[i];
+        }
+        for(let i = 0; i<30000; i++){
+            setTimeout(function(){targetElement.innerHTML = Math.floor(Math.random()*900) + 100;}, 100)
+        }
+        this.target = Math.floor(Math.random()*900) + 100;
+        targetElement.innerHTML = this.target;
+        this.resetAllSlots();
+    }
+
+    disableAllSlots(){
+        for (let slot of this.slotElement){
+            if(!slot.classList.contains("disabled"))
+            slot.classList.add("disabled");
+        }
+    }
+
+    resetAllSlots(){
+        for (let slot of this.slotElement){
+            if(slot.classList.contains("disabled"))
+            slot.classList.remove("disabled");
+            if(slot.classList.contains("selected"))
+            slot.classList.remove("selected");
+            if(slot.classList.contains("comp"))
+            slot.classList.remove("comp");
+        }
+    }
+
+    disableSlot(element){
+        if(!element.classList.contains("disabled"))
+            element.classList.add("disabled");
+    }
+
+    enableSlot(element){
+        if(element.classList.contains("disabled"))
+            element.classList.remove("disabled");
+    }  
+    
+    selectSlot(element){
+        if(!element.classList.contains("selected"))
+            element.classList.add("selected");
+    }
+
+    unselectSlot(element){
+        if(element.classList.contains("selected"))
+            element.classList.remove("selected");
+    }
+
+    replaceCard(slot1, slot2, newNum){
+        if(slot1.classList.contains("comp"))
+        slot1.classList.remove("comp");
+        this.unselectSlot(slot1);
+        this.disableSlot(slot1);
+        this.selectSlot(slot2);
+        slot1.innerHTML = "";
+        slot2.innerHTML = newNum;
+        if(!slot2.classList.contains("comp"))
+            slot2.classList.add("comp");
+    }
+
+}
+
+class Operator {
+
+    constructor(){
+        this.operatorElement = document.getElementsByClassName("operator");
+    }
+
+
+    disableAllOperators(){
+        for (let operator of this.operatorElement){
+            if(!operator.classList.contains("disabled"))
+            operator.classList.add("disabled");
+        }
+    }
+
+    enableAllOperators(){
+        for (let operator of this.operatorElement){
+            if(operator.classList.contains("disabled"))
+            operator.classList.remove("disabled");
+        }
+    }
+
+    isValidOperation(num1, Op, num2){
+        if (Op=="-") {
+            return (num1 > num2);
+        }
+        if (Op=="÷") {
+            return (num1 % num2 == 0);
+        }
+        return true;
+    }
+
+    operate(slot1, operator, slot2) {
+        let num1 = parseInt(slot1.innerHTML), Op = operator.innerHTML, num2 = parseInt(slot2.innerHTML);
+        if (!this.isValidOperation(num1, Op, num2))
+            return false;
+        switch (Op){
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "×":
+                return num1 * num2;
+            case "÷":
+                return num1 / num2;
+                                        
+        }
+    }
+}
+
+class HistoryRecorder {
+    constructor(){
+        this.cardArchive = [];
+        this.scratchArchive = [];
+        this.archiveStatus = 0;
+    }
+
+    archiveScratch(scratchString,){
+        this.scratchArchive.push(scratchString);
+    }
+
+    archiveCard(cardArray){
+        this.cardArchive.push(cardArray);
+    }
+
+
+}
+
+class ScratchBoard {
+    constructor(){
+        this.scratchLines = [];
+        this.scratchBoardElement = document.getElementsByClassName("scratch")[0];
+    }
+
+    addScratch(text){
+        this.scratchBoardElement.innerHTML += text;
+    }
+    undoScratch(){
+
+    }    
+
+    clearScratch(){
+        this.scratchBoardElement.innerHTML = "";
+    }
+
+}
+
+class Gameboard {
+
+    constructor(){
+        this.state = 0;
+        // state could be:
+        // 0 - No ongoing game. e.g. initial state. Disable all buttons.
+        // 1 - Ongoing game, no element selected. Disable all operators.
+        // 2 - Ongoing game, first operand selected.
+        // 3 - Ongoing game, operator selected. Disable all operators.
+        // 4 - Ongoing game, second operand selected and proceed to check.
+        this.card = new NumberSlot();
+        this.scratchBoard = new ScratchBoard();
+        this.history = new HistoryRecorder();
+        this.operator = new Operator();
+        this.operandHeld = false;
+        this.operatorHeld = false;
+        this.func = document.getElementsByClassName("func");
+    }
+    
+    reset(){
+        this.card.retry();
+        this.state = 1;    
+        this.scratchBoard.clearScratch();  
+        this.card.resetAllSlots();
+        this.operandHeld = false;
+        this.operatorHeld = false;
+        this.processState();
+    }
+
+    newGame(numBigCards = -1){
+        this.card.setSlot(numBigCards);
+        this.state = 1;    
+        this.scratchBoard.clearScratch();  
+        this.operandHeld = false;
+        this.operatorHeld = false;
+        this.func[1].classList.remove("disabled");
+        this.processState();
+    }
+
+    checkGame(){
+
+
+    }
+
+    processState(){
+        switch (this.state) {
+            case 0:
+                this.operator.disableAllOperators();
+                break;
+            case 1:
+                this.operator.disableAllOperators();
+                break;
+            case 2:
+                this.operator.enableAllOperators();
+                break;
+            case 3:
+                this.operator.disableAllOperators();          
+                break;
+
+            
+        }
+    }
+
+    onclick(element){
+        switch (this.state) {
+            case 1:
+                this.card.selectSlot(element);
+                this.state++;
+                this.operandHeld = element;
+                this.processState();
+                break;
+            case 2:
+                if (element.classList.contains("number")){
+                    this.card.selectSlot(element);
+                    this.card.unselectSlot(this.operandHeld);
+                    this.operandHeld = element;
+                } else {
+                    this.scratchBoard.addScratch(this.operandHeld.innerHTML + element.innerHTML);
+                    this.state++;
+                    this.operatorHeld = element;
+                    this.processState();
+                    break;
+                }
+                break;
+            case 3:
+                /*if(element == this.operandHeld){
+                    this.operandHeld = false;
+                    this.state=1;
+                    this.processState();
+                }*/
+                if(this.operandHeld==element)
+                    break;
+                let result = this.operator.operate(this.operandHeld, this.operatorHeld, element);
+                if(result){
+                    this.scratchBoard.addScratch(element.innerHTML);
+                    this.scratchBoard.addScratch("=" + result + "<br>");
+                    this.state=2; 
+                    this.card.replaceCard(this.operandHeld, element, result);
+                    this.operandHeld = element;
+                    this.operatorHeld = false;
+                    this.processState();
+                }
+                break;
+            case 4:
+                break;
+        }
+    }
+
+}
+
+var board = new Gameboard();
